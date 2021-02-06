839012c008e6eb88b5b0ba04857713247873bde7
maratrix <maratrix@163.com>
2021-02-06 21:58:57 +0800

    add ip utils

---
+++ ip/addr_test.go
@@ -0,0 +1,16 @@
+package ip
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInternalIp(t *testing.T) {
+	ip := InternalIp()
+	assert.NotEqual(t, "", ip)
+
+	if ip == "127.0.0.1" {
+		t.Errorf("loopback not excluded")
+	}
+}
---
+++ ip/convert_test.go
@@ -0,0 +1,13 @@
+package ip
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIpConvert(t *testing.T) {
+	addr := "192.34.45.4"
+	assert.Equal(t, uint32(0xc0222d04), Ip2long(addr))
+	assert.Equal(t, "192.34.45.4", Long2ip(0xc0222d04))
+}
